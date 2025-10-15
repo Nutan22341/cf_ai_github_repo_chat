128d21e8134d6e3bc4fbe2afd01521d8b6cc1c0a
Nutan22341 <nutan22341@iiitd.ac.in>
2025-10-15 14:44:49 +0530

    updated the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        
         <Route path="/" element={<HomePage />} />
         <Route path="/chat" element={<ChatPage />} />
       </Routes>
